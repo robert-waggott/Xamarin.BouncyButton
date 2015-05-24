b6a67cf93dfb1cb8309b62443be3b19c4e1300a6
robert.waggott <robert.waggott@gmail.com>
2015-05-24 16:12:40 +0100

    beginning to add the component

---
+++ BouncyButton/BouncyButton/UIBouncyButton.cs
@@ -0,0 +1,37 @@
+﻿namespace BouncyButton
+{
+	using System;
+
+	using CoreGraphics;
+	using UIKit;
+
+	public class UIBouncyButton : UIButton
+	{
+		public UIBouncyButton()
+			: base()
+		{
+			this.TouchUpInside += this.OnTouch;
+		}
+
+		public UIBouncyButton(UIButtonType type)
+			: base(type)
+		{
+		}
+
+		public UIBouncyButton(CGRect frame)
+			: base(frame)
+		{
+		}
+
+		public Action BeforeAnimation { get; set; }
+
+		public Action DuringAnimation { get; set; }
+
+		public Action AfterAnimation { get; set; }
+
+		protected void OnTouch(object sender, EventArgs e)
+		{
+					
+		}
+	}
+}
