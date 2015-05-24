b19b83127246e987899665ef4f094f527a0af9d6
robert.waggott <robert.waggott@gmail.com>
2015-05-24 21:13:03 +0100

    authoring the component

--- BouncyButton/BouncyButton/UIBouncyButton.cs
+++ BouncyButton/BouncyButton/UIBouncyButton.cs
@@ -16,11 +16,13 @@
 		public UIBouncyButton(UIButtonType type)
 			: base(type)
 		{
+			this.TouchUpInside += this.OnTouch;
 		}
 
 		public UIBouncyButton(CGRect frame)
 			: base(frame)
 		{
+			this.TouchUpInside += this.OnTouch;
 		}
 
 		public Action BeforeAnimation { get; set; }
@@ -29,9 +31,36 @@
 
 		public Action AfterAnimation { get; set; }
 
-		protected void OnTouch(object sender, EventArgs e)
+		protected virtual void OnTouch(object sender, EventArgs e)
 		{
-					
+			if (this.BeforeAnimation != null) 
+			{
+				this.BeforeAnimation.Invoke();
+			}
+
+			Animate(0.3, () => this.Transform = CGAffineTransform.MakeScale(0.83f, 0.83f));
+			Animate(0.2, () => this.Transform = CGAffineTransform.MakeScale(0.86f, 0.86f));
+			Animate(0.18, 
+				() => this.Transform = CGAffineTransform.MakeScale(0.85f, 0.85f), 
+				() => this.Highlighted = true);
+
+			if (this.DuringAnimation != null)
+			{
+				this.DuringAnimation.Invoke();
+			}
+
+			Animate(0.1, 
+				() => this.Transform = CGAffineTransform.MakeScale(0.85f, 0.85f),
+				() => this.Highlighted = false);
+			Animate(0.18, () => this.Transform = CGAffineTransform.MakeScale(1.05f, 1.05f));
+			Animate(0.18, () => this.Transform = CGAffineTransform.MakeScale(0.98f, 0.98f));
+			Animate(0.17, () => this.Transform = CGAffineTransform.MakeScale(1.01f, 1.01f));
+			Animate(0.17, () => this.Transform = CGAffineTransform.MakeIdentity());
+
+			if (this.AfterAnimation != null)
+			{
+				this.AfterAnimation.Invoke();
+			}
 		}
 	}
 }
